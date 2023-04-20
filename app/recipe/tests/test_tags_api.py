b750bacb66982a5e0d49418d78775a9ac1baf0cb
oguzhanoxel <oguzhanoksel@hotmail.com>
2023-04-20 15:26:59 +0300

    added ingredients model, implemented ingredients api

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -16,7 +16,7 @@ from recipe.serializers import TagSerializer
 TAGS_URL = reverse('recipe:tag-list')
 
 
-def  detail_url(tag_id):
+def detail_url(tag_id):
     """Create and return a tag detail url."""
     return reverse('recipe:tag-detail', args=[tag_id])
 
